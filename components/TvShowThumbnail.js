9a5c3e72282f78044b7934bd69090feb3b4f15ae
ariuka11 <ariun1118@gmail.com>
2022-01-21 12:40:49 -0500

    Finished making movie component

--- components/TvShowThumbnail.js
+++ components/TvShowThumbnail.js
@@ -6,7 +6,10 @@ const TvShowThumbnail = ({ res }) => {
   const router = useRouter();
 
   return (
-    <div className="flex min-w-[200px] min-h-[300px] md:min-w-[250px] md:min-h-[380px] overflow-hidden shadow-xl rounded-lg cursor-pointer border-4 border-secondary border-opacity-5 hover:border-opacity-80 hover:shadow-2xl transform hover:scale-105 transition duration-300 my-2">
+    <div
+      className="flex min-w-[200px] min-h-[300px] md:min-w-[250px] md:min-h-[380px] overflow-hidden shadow-xl rounded-lg cursor-pointer border-4 border-secondary border-opacity-5 hover:border-opacity-80 hover:shadow-2xl transform hover:scale-105 transition duration-300 my-2"
+      onClick={() => router.push(`/show/${res.id}`)}
+    >
       <Image
         src={`${url}${res.poster_path || res.backdrop_path}`}
         width={330}
